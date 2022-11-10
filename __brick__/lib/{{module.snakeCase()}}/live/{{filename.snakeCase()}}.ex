f10b3f3301f02cc1cf67b257e337114bcb9c812e
Pedro Luz <pedromsluz@gmail.com>
2022-11-10 23:05:20 +0000

    Initial

---
+++ __brick__/lib/{{module.snakeCase()}}/live/{{filename.snakeCase()}}.ex
@@ -0,0 +1,14 @@
+defmodule {{module}}.{{filename}} do
+  @moduledoc """
+    {{module}}.{{filename}}
+  """
+
+  use Surface.LiveView
+
+  def mount(_params, _uri, socket) do
+    {
+      :ok,
+      socket
+    }
+  end
+end
